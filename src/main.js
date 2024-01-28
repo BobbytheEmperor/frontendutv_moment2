0a77ccb15cb5ada49466d94db203f1d75bef0b61
Pauline <pasp2300@student.miun.se>
2024-01-28 17:24:35 +0100

    Funktionalitet för sortering av data tillaggd

--- src/main.js
+++ src/main.js
@@ -2,24 +2,32 @@
 
 //Funktionen getdata är funktionen innehållandes de andra funktionerna. 
 
-async function getdata() {
+async function getdata(sortProperty) {
 
-    //Skapar de första funktionerna för inhämtning av data.
-    //Använder async och await för att göra funktionen asynkron. 
 
     const data = await fetch("https://dahlgren.miun.se/ramschema_ht23.php").then(resp => {
         return resp.json()
     })
 
     //Sortering av kursnamn i alfabetisk ordning genom funktionerna sort och .localeCompare.
+    //Funktion för sortering skapad. 
 
     data.sort((a, b) => {
-        const result = a.coursename.localeCompare(b.coursename);
-        return result;
+        switch (sortProperty) {
+            case "code":
+                return a.code.localeCompare(b.code);
+            case "coursename":
+                return a.coursename.localeCompare(b.coursename);
+            case "progression":
+                return a.progression.localeCompare(b.progression);
+            default:
+                return 0;
+        }
     })
 
 
     const tbody = document.getElementById("tableBody");
+    tbody.innerHTML = "";
     for (const info of data) {
         const tr = document.createElement("tr");
         const tdCode = document.createElement("td");
@@ -37,3 +45,6 @@ async function getdata() {
 }
 getdata();
 
+document.getElementById("code").addEventListener("click", () => getdata("code"));
+document.getElementById("course").addEventListener("click", () => getdata("coursename"));
+document.getElementById("prog").addEventListener("click", () => getdata("progression"));
