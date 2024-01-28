a9dad76da93b1ef15a4d9782b3411599912f2cbc
Pauline <pasp2300@student.miun.se>
2024-01-28 16:42:16 +0100

    Använt async och await för att skapa en asynkron funktion.

--- src/main.js
+++ src/main.js
@@ -2,29 +2,30 @@
 
 //Funktionen getdata är funktionen innehållandes de andra funktionerna. 
 
-function getdata() {
+async function getdata() {
 
     //Skapar de första funktionerna för inhämtning av data.
-    //Async, Ajax-anrop samt sortering har ännu ej gjorts. 
+    //Ajax-anrop samt sortering har ännu ej gjorts. 
 
-    fetch("https://dahlgren.miun.se/ramschema_ht23.php").then(resp => {
+    const data = await fetch("https://dahlgren.miun.se/ramschema_ht23.php").then(resp => {
         return resp.json()
-    }).then(data => {
-        const tbody = document.getElementById("tableBody");
-        for (const info of data) {
-            const tr = document.createElement("tr");
-            const tdCode = document.createElement("td");
-            tr.appendChild(tdCode);
-            tdCode.innerText = info.code;
-            const tdName = document.createElement("td");
-            tr.appendChild(tdName);
-            tdName.innerText = info.coursename;
-            const tdProg = document.createElement("td");
-            tr.appendChild(tdProg);
-            tdProg.innerText = info.progression;
-            tbody.appendChild(tr);
-        }
     })
+
+    const tbody = document.getElementById("tableBody");
+    for (const info of data) {
+        const tr = document.createElement("tr");
+        const tdCode = document.createElement("td");
+        tr.appendChild(tdCode);
+        tdCode.innerText = info.code;
+        const tdName = document.createElement("td");
+        tr.appendChild(tdName);
+        tdName.innerText = info.coursename;
+        const tdProg = document.createElement("td");
+        tr.appendChild(tdProg);
+        tdProg.innerText = info.progression;
+        tbody.appendChild(tr);
+    }
+
 }
 getdata();
 
