8aa7552220494ad087615249efcbf0ff27089a12
Pauline <pasp2300@student.miun.se>
2024-01-29 10:12:35 +0100

    Testar om fel är åtgärdat

--- src/main.js
+++ src/main.js
@@ -1,30 +1,61 @@
+
 "use strict"
 
 //Funktionen getdata är funktionen innehållandes de andra funktionerna. 
 
-function getdata() {
+async function getdata(sortProperty, filterString) {
 
-    //Skapar de första funktionerna för inhämtning av data.
-    //Async, Ajax-anrop samt sortering har ännu ej gjorts. 
 
-    fetch("https://dahlgren.miun.se/ramschema_ht23.php").then(resp => {
+    let data = await fetch("https://dahlgren.miun.se/ramschema_ht23.php").then(resp => {
         return resp.json()
-    }).then(data => {
-        const tbody = document.getElementById("tableBody");
-        for (const info of data) {
-            const tr = document.createElement("tr");
-            const tdCode = document.createElement("td");
-            tr.appendChild(tdCode);
-            tdCode.innerText = info.code;
-            const tdName = document.createElement("td");
-            tr.appendChild(tdName);
-            tdName.innerText = info.coursename;
-            const tdProg = document.createElement("td");
-            tr.appendChild(tdProg);
-            tdProg.innerText = info.progression;
-            tbody.appendChild(tr);
+    })
+
+    //Sortering av kursnamn i alfabetisk ordning genom funktionerna sort och .localeCompare.
+    //Funktion för sortering skapad. 
+
+    data.sort((a, b) => {
+        switch (sortProperty) {
+            case "code":
+                return a.code.localeCompare(b.code);
+            case "coursename":
+                return a.coursename.localeCompare(b.coursename);
+            case "progression":
+                return a.progression.localeCompare(b.progression);
+            default:
+                return 0;
         }
     })
+
+    data = data.filter(a => {
+        if(!filterString){
+            return true;
+        }
+            const matchCode = a.code.toLowerCase().includes(filterString.toLowerCase());
+            const matchName = a.coursename.toLowerCase().includes(filterString.toLowerCase());
+            return matchCode || matchName;
+    })
+
+    const tbody = document.getElementById("tableBody");
+    tbody.innerHTML = "";
+    for (const info of data) {
+        const tr = document.createElement("tr");
+        const tdCode = document.createElement("td");
+        tr.appendChild(tdCode);
+        tdCode.innerText = info.code;
+        const tdName = document.createElement("td");
+        tr.appendChild(tdName);
+        tdName.innerText = info.coursename;
+        const tdProg = document.createElement("td");
+        tr.appendChild(tdProg);
+        tdProg.innerText = info.progression;
+        tbody.appendChild(tr);
+    }
+
 }
 getdata();
 
+document.getElementById("code").addEventListener("click", () => getdata("code"));
+document.getElementById("course").addEventListener("click", () => getdata("coursename"));
+document.getElementById("prog").addEventListener("click", () => getdata("progression"));
+
+document.getElementById("filter").addEventListener("input", (event) => getdata("", event.target.value) );
